f3f87d08968da08550ec06eb50dad18c564f1ac2
Bolade <boladebode@gmail.com>
2020-06-22 14:29:00 +0100

    db added

--- config/knex.js
+++ config/knex.js
@@ -3,15 +3,29 @@ exports.knex = require( 'knex' )( {
 
   client: 'mysql',
   connection: { 
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'bcommerce',
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b630dcb44f737e',
+    password: '76bc6049',
+    database: 'heroku_0af4a054f44f63f',
     charset: 'utf8',
 
   }
 
-} );
+} ); 
+
+// exports.knex = require( 'knex' )( {
+
+//   client: 'mysql',
+//   connection: { 
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: '',
+//     database: 'bcommerce',
+//     charset: 'utf8',
+
+//   }
+
+// } );
 // exports.knex = require( 'knex' )( {
 
 //   client: 'mysql',
@@ -26,4 +40,4 @@ exports.knex = require( 'knex' )( {
 
 // } );
 
- 
+ // mysql://b630dcb44f737e:76bc6049@us-cdbr-east-05.cleardb.net/heroku_0af4a054f44f63f?reconnect=true

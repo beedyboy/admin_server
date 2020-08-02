bf51d65fafb75967013029dd376b26bf2449d490
Bolade <boladebode@gmail.com>
2020-08-02 11:36:14 +0100

    make it better

--- config/knex.js
+++ config/knex.js
@@ -1,18 +1,21 @@
 
+ 
 
 exports.knex = require( 'knex' )( {
 
   client: 'mysql',
   connection: { 
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'bcommerce',
+    host: '209.205.209.130',
+    user: 'devprima_noroot',
+    password: '&CI@bQ^d0hTm',
+    database: 'devprima_bcommerce',
     charset: 'utf8',
 
   }
 
 } );
+
+  
 // exports.knex = require( 'knex' )( {
 
 //   client: 'mysql',

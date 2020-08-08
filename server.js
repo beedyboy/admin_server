4d4e1b60a9192c026b87e32b18d3c3b59ed79a05
Bolade <boladebode@gmail.com>
2020-08-08 22:19:32 +0100

    cors updated

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ const app = express();
 
 // app.use(cors());
 app.all('*', function(req, res, next) {
-	//  var origin = req.get('origin'); 
-	var origin = "https://admin-commerce.herokuapp.com";
+	 var origin = req.get('origin'); 
+	// var origin = "https://admin-commerce.herokuapp.com";
      res.header('Access-Control-Allow-Origin', origin); 
 	 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS'); 
-	 res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization");
- 
+	 res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization"); 
      next();
 });
 app.use('/uploads', express.static('uploads'));

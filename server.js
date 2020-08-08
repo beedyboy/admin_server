33e6f81452e78621f9fe280aae5eedf428b72f75
Bolade <boladebode@gmail.com>
2020-08-08 17:28:40 +0100

    cors updated

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://admin-commerce.herokuapp.com");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-		return res.status(200).json({});
-	}
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header("Access-Control-Allow-Origin", "https://admin-commerce.herokuapp.com");
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+// 	if (req.method === 'OPTIONS') {
+// 		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+// 		return res.status(200).json({});
+// 	}
+// 	next();
+// });
 
 app.use('/api', routes); 
 app.get('/', (req,res) => { 

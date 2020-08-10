bd7c99071ab792114ea3d9a9928800dbb914437b
Bolade <boladebode@gmail.com>
2020-08-10 10:04:16 +0100

    cors updated

--- server.js
+++ server.js
@@ -2,28 +2,32 @@ const express = require("express");
 const bodyParser = require('body-parser'); 
 const path = require('path'); 
 const fs = require('fs'); 
-const cors = require("cors"); 
+// const cors = require("cors"); 
 var routes = require('./models/index');
 // var sms = require('./plugins/sms');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-app.options('*', cors());
-
- 
+// app.options('*', cors());
 
  
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*"); 
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
-	res.header("Access-Control-Allow-Headers", "x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization");
-	if (req.method === 'OPTIONS') { 
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, OPTIONS, PATCH, DELETE, GET');
-		return res.status(200).json({});
-	}
-	next();
-});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://admin-commerce.herokuapp.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+ 
+// app.use((req, res, next) => {
+// 	res.header("Access-Control-Allow-Origin", "*"); 
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
+// 	res.header("Access-Control-Allow-Headers", "x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+// 	if (req.method === 'OPTIONS') { 
+// 		res.header('Access-Control-Allow-Methods', 'PUT, POST, OPTIONS, PATCH, DELETE, GET');
+// 		return res.status(200).json({});
+// 	}
+// 	next();
+// });
 
 app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));

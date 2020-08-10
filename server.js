58108a6d6c8752374976678d8f24dd7a0c4bbe81
Bolade <boladebode@gmail.com>
2020-08-10 09:18:21 +0100

    cors

--- server.js
+++ server.js
@@ -44,16 +44,16 @@ app.use(bodyParser.json());
 // });
 
 
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://admin-commerce.herokuapp.com"); 
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
-	res.header("Access-Control-Allow-Headers", "x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization");
-	if (req.method === 'OPTIONS') { 
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, OPTIONS, PATCH, DELETE, GET');
-		return res.status(200).json({});
-	}
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header("Access-Control-Allow-Origin", "https://admin-commerce.herokuapp.com"); 
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
+// 	res.header("Access-Control-Allow-Headers", "x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+// 	if (req.method === 'OPTIONS') { 
+// 		res.header('Access-Control-Allow-Methods', 'PUT, POST, OPTIONS, PATCH, DELETE, GET');
+// 		return res.status(200).json({});
+// 	}
+// 	next();
+// });
 app.use('/api', routes); 
 app.get('/', (req,res) => { 
  res.writeHead(200, {'Content-Type': 'text/plain'});

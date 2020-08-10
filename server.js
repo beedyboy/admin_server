c4d752f68fe4f199d768cb2a40d3eb1ed78920e7
Bolade <boladebode@gmail.com>
2020-08-10 09:13:09 +0100

    cors

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
 
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*"); 
+	res.header("Access-Control-Allow-Origin", "https://admin-commerce.herokuapp.com"); 
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
 	res.header("Access-Control-Allow-Headers", "x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	if (req.method === 'OPTIONS') { 
